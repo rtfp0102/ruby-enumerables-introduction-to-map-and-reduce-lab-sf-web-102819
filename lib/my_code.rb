7431d32b74ea72948a3de1a440aa7b1c81337a0c
rtfp0102 <rtfp0102@gmail.com>
2019-10-09 19:32:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -64,3 +64,16 @@ def reduce_to_total(source_array, starting_point = 0)
   return total 
 end
 
+def reduce_to_all_true(source_array) 
+  i = 0 
+  while  i < source_array.length do
+    ele = source_array[i]
+    
+    if ele == false
+      return false 
+    end
+    
+    i += 1 
+  end
+  return true
+end
